cde8e8c225bda993f933cdde2651ebcf8657ea21
JQDOM <Seattlepowers@gmail.com>
2017-02-23 14:05:33 -0800

    merging master to adampage

---
+++ js.js
@@ -0,0 +1,12 @@
+document.getElementByClass("imgIcon").onmouseover(icon1)
+
+
+function icon1(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+}
+
+function icon2(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+}
